518f28b2d7841a106f42d11aef31ad8096ea6c80
Tim te Beek <ttebeek@bio-prodict.nl>
2009-03-18 13:49:24 +0000

    doa

--- dao/persistance/connect/Connector.java
+++ dao/persistance/connect/Connector.java
@@ -6,35 +6,33 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public abstract class Connector {
-	private static Connection dbconnection = null;
+	private static Connection	dbconnection	= null;
+	private static Properties	dbconfig		= null;
 
 	public static void connect(Properties dbconf) throws DBConnectionException {
 		if (dbconf == null)
-			throw new DBConnectionException("Invalid JDBC Connection settings supplied in Connector.connect();",null);
+			throw new DBConnectionException("Invalid JDBC Connection settings supplied in Connector.connect();", null);
+		dbconfig = dbconf;
 
 		try {
 			Class.forName(dbconf.getProperty("driver"));
-			Connector.dbconnection = DriverManager.getConnection(dbconf
-					.getProperty("protocol")
-					+ "//"
-					+ dbconf.getProperty("host")
-					+ ":"
-					+ dbconf.getProperty("port")
-					+ "/"
-					+ dbconf.getProperty("name"), dbconf.getProperty("user"),
-					dbconf.getProperty("pass"));
-		} catch (ClassNotFoundException e) {
+			Connector.dbconnection = DriverManager.getConnection(dbconf.getProperty("protocol") + "//" + dbconf.getProperty("host") + ":" + dbconf.getProperty("port") + "/" + dbconf.getProperty("name"), dbconf.getProperty("user"), dbconf.getProperty("pass"));
+		}
+		catch (ClassNotFoundException e) {
 			Connector.dbconnection = null;
 			throw new DBConnectionException("Could not load jdbcdriver!", e);
-		} catch (SQLException e) {
+		}
+		catch (SQLException e) {
 			Connector.dbconnection = null;
 			throw new DBConnectionException("Could not connect to database!", e);
 		}
 	}
 
-	public static Connection getConnection() throws DBConnectionException {
-		if (Connector.dbconnection == null)
-			throw new DBConnectionException("RDB not connected!",null);
+	public static Connection getConnection() throws DBConnectionException, SQLException {
+		if (Connector.dbconnection == null || !Connector.dbconnection.isValid(0))
+			connect(dbconfig);
+		if (Connector.dbconnection == null || !Connector.dbconnection.isValid(0))
+			throw new DBConnectionException("RDB not connected!", null);
 		return Connector.dbconnection;
 	}
 
@@ -43,7 +41,8 @@ public abstract class Connector {
 		try {
 			if (Connector.dbconnection != null)
 				Connector.dbconnection.close();
-		} finally {
+		}
+		finally {
 			super.finalize();
 		}
 	}

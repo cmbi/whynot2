803e8b1c5d4a186be4c13e96ffcf247586c3599d
Tim te Beek <ttebeek@bio-prodict.nl>
2009-03-27 15:57:03 +0000

    Emptying trunk after tagging everything as 1.2

--- WHY_NOT/WebContent/page_errors.jsp
+++
@@ -1,6 +0,0 @@
-<%@ taglib prefix="s" uri="/struts-tags" %>
-<s:if test="hasErrors()">
-<s:div id="errors">
-	<s:actionerror/>
-</s:div>
-</s:if>
--- dao/persistance/PersistanceException.java
+++
@@ -1,8 +0,0 @@
-package persistance;
-
-@SuppressWarnings("serial")
-public class PersistanceException extends Exception {
-	protected PersistanceException(String message, Throwable cause) {
-		super(message, cause);
-	};
-}

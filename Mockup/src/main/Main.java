8c0237c302155e01097b3660056d03eeb751a793
Tim te Beek <ttebeek@bio-prodict.nl>
2009-04-01 22:26:03 +0000

    Initial import of mockup: creates bogus files to index

---
+++ Mockup/src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		String dir = "raw/";
+		if (!new File(dir).isDirectory())
+			new File(dir).mkdir();
+		
+		for (File f : Arrays.asList(new File(dir).listFiles()))
+			f.delete();
+		
+		String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		Random r = new Random();
+		for (int i = 0; i < 5; i++)
+			new File(dir+"pdb" + r.nextInt(10) + abc.charAt(r.nextInt(26)) + abc.charAt(r.nextInt(26)) + abc.charAt(r.nextInt(26))+".ent").createNewFile();
+		for (int i = 0; i < 5; i++)
+			new File(dir+"" + r.nextInt(10) + abc.charAt(r.nextInt(26)) + abc.charAt(r.nextInt(26)) + abc.charAt(r.nextInt(26))+".dssp").createNewFile();
+	}
+
+}

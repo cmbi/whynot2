14e84446bdaf9ae11222d77764f0abc37b896204
Tim te Beek <ttebeek@bio-prodict.nl>
2009-04-17 18:28:51 +0000

    Added setup for session management

--- WHY_NOT/src/test/CommenterTest.java
+++ WHY_NOT/src/test/CommenterTest.java
@@ -4,33 +4,49 @@ import java.io.File;
 
 import junit.framework.Assert;
 
+import org.hibernate.Session;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
 import comment.Commenter;
 
+import dao.hibernate.DAOFactory;
+import dao.hibernate.HibernateUtil;
 
 public class CommenterTest {
-	String	comfile		= "comment/com1.txt";
-	String	uncomfile	= "uncomment/uncom1.txt";
+	DAOFactory	factory;
+	Session		session;
 
 	@Before
-	public void setUp() throws Exception {}
+	public void setUp() throws Exception {
+		factory = DAOFactory.instance(DAOFactory.HIBERNATE);
+		factory.setSession(HibernateUtil.getSessionFactory().openSession());
+		session = factory.getSession();
+	}
 
 	@After
 	public void tearDown() throws Exception {
-		new File(comfile + ".done").renameTo(new File(comfile));
-		new File(uncomfile + ".done").renameTo(new File(uncomfile));
+		session.close();
 	}
+
+	static final String	comfile		= "comment/com1.txt";
+	static final String	uncomfile	= "uncomment/uncom1.txt";
 
 	//@Test
 	public void comment() throws Exception {
-		Assert.assertTrue(Commenter.comment(comfile));
+		Assert.assertTrue(Commenter.comment(CommenterTest.comfile));
 	}
 
 	@Test
 	public void uncomment() throws Exception {
-		Assert.assertTrue(Commenter.uncomment(uncomfile));
+		Assert.assertTrue(Commenter.uncomment(CommenterTest.uncomfile));
+	}
+
+	@AfterClass
+	public static void resetfiles() {
+		new File(CommenterTest.comfile + ".done").renameTo(new File(CommenterTest.comfile));
+		new File(CommenterTest.uncomfile + ".done").renameTo(new File(CommenterTest.uncomfile));
 	}
 }

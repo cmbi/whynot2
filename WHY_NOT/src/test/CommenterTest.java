a46ef4ca3b4b213ef432f4c1cb9ef659ee06be4b
Tim te Beek <ttebeek@bio-prodict.nl>
2009-04-20 08:52:08 +0000

    Commenter working with currentsession

--- WHY_NOT/src/test/CommenterTest.java
+++ WHY_NOT/src/test/CommenterTest.java
@@ -4,36 +4,16 @@ import java.io.File;
 
 import junit.framework.Assert;
 
-import org.hibernate.Session;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.Test;
 
 import comment.Commenter;
 
-import dao.hibernate.DAOFactory;
-
 public class CommenterTest {
-	DAOFactory	factory;
-	Session		session;
-
-	@Before
-	public void setUp() throws Exception {
-		factory = DAOFactory.instance(DAOFactory.HIBERNATE);
-		//factory.setSession(HibernateUtil.getSessionFactory().openSession());
-		session = factory.getSession();
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		session.close();
-	}
-
 	static final String	comfile		= "comment/com1.txt";
 	static final String	uncomfile	= "uncomment/uncom1.txt";
 
-	//@Test
+	@Test
 	public void comment() throws Exception {
 		Assert.assertTrue(Commenter.comment(CommenterTest.comfile));
 	}

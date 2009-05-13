c5830fae651bd95e9c11672b5c58572d24faf0b2
Tim te Beek <ttebeek@bio-prodict.nl>
2009-05-13 10:59:32 +0000

    Still need to configure Hibernate Session in web xml through another filter I guess

--- why_not-web/src/main/java/nl/ru/cmbi/why_not/HomePage.java
+++ why_not-web/src/main/java/nl/ru/cmbi/why_not/HomePage.java
@@ -1,29 +1,45 @@
 package nl.ru.cmbi.why_not;
 
+import nl.ru.cmbi.why_not.hibernate.GenericDAO.EntryDAO;
+
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 /**
  * Homepage
  */
 public class HomePage extends WebPage {
 
-	private static final long serialVersionUID = 1L;
+	private static final long	serialVersionUID	= 1L;
 
 	// TODO Add any page properties or variables here
 
-    /**
+	@SpringBean
+	//@Autowired
+	private EntryDAO			entrydao;
+
+	/**
 	 * Constructor that is invoked when page is invoked without a session.
 	 * 
 	 * @param parameters
 	 *            Page parameters
 	 */
-    public HomePage(final PageParameters parameters) {
+	public HomePage(final PageParameters parameters) {
 
-        // Add the simplest type of label
-        add(new Label("message", "If you see this message wicket is properly configured and running"));
+		// Add the simplest type of label
+		add(new Label("message", "If you see this message wicket is properly configured and running"));
+		add(new Label("count", new Model<String>() {
+			@Override
+			public String getObject() {
+				//if (entrydao != null)
+				return "" + entrydao.countAll();
+				//return "Its still NULL dummy!";
+			}
+		}));
 
-        // TODO Add your page's components here
-    }
+		// TODO Add your page's components here
+	}
 }
--- why_not-web/src/main/java/nl/ru/cmbi/why_not/WicketApplication.java
+++ why_not-web/src/main/java/nl/ru/cmbi/why_not/WicketApplication.java
@@ -1,26 +1,24 @@
 package nl.ru.cmbi.why_not;
 
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
  * 
  * @see nl.ru.cmbi.why_not.Start#main(String[])
  */
-public class WicketApplication extends WebApplication
-{    
-    /**
-     * Constructor
-     */
-	public WicketApplication()
-	{
+public class WicketApplication extends WebApplication {
+	@Override
+	protected void init() {
+		addComponentInstantiationListener(new SpringComponentInjector(this));
 	}
-	
+
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
-	public Class<HomePage> getHomePage()
-	{
+	@Override
+	public Class<HomePage> getHomePage() {
 		return HomePage.class;
 	}
 

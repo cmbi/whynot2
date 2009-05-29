79824228f8896778cdfbabc39243157889253a1b
Tim te Beek <ttebeek@bio-prodict.nl>
2009-05-29 16:56:02 +0000

    Half finished new lister (much simpeler and less extensive)

--- whynot-core/src/main/java/nl/ru/cmbi/whynot/list/Lister.java
+++ whynot-core/src/main/java/nl/ru/cmbi/whynot/list/Lister.java
@@ -4,6 +4,7 @@ import java.util.SortedSet;
 
 import nl.ru.cmbi.whynot.hibernate.SpringUtil;
 import nl.ru.cmbi.whynot.hibernate.GenericDAO.DatabankDAO;
+import nl.ru.cmbi.whynot.hibernate.GenericDAO.EntryDAO;
 import nl.ru.cmbi.whynot.model.Databank;
 import nl.ru.cmbi.whynot.model.Entry;
 
@@ -14,8 +15,40 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class Lister {//TODO Valid, Obsolete, Missing, With / Without
-	public static void main(String[] args) throws Exception {
+public class Lister {
+	private enum SelectionType {
+		VALID, OBSOLETE, MISSING, ANNOTATED, UNANNOTATED
+	};
+
+	public static void main(String... args) {
+		String dbname = "DATABASE";
+		String selection = "VALID|OBSOLETE|MISSING|ANNOTATED|UNANNOTATED";
+		if (args.length != 2 || !args[0].matches(dbname) || !args[1].matches(selection))
+			throw new IllegalArgumentException("Usage: lister " + dbname + " " + selection);
+
+		Lister lister = (Lister) SpringUtil.getContext().getBean("lister");
+		lister.list(dbname, SelectionType.valueOf(selection));
+	}
+
+	@Autowired
+	EntryDAO	entdao;
+
+	private void list(String dbname, SelectionType selection) {
+		switch (selection) {//TODO
+		case VALID:
+
+		case OBSOLETE:
+
+		case MISSING:
+
+		case ANNOTATED:
+
+		case UNANNOTATED:
+
+		}
+	}
+
+	public static void main_old(String[] args) throws Exception {
 		String dbname = "DATABASE";
 		String fileFilter = "withFile|withoutFile";
 		String parentFilter = "withParentFile|withoutParentFile";
@@ -23,7 +56,7 @@ public class Lister {//TODO Valid, Obsolete, Missing, With / Without
 		String comment = "[\"Example comment\"]";
 
 		if (args.length != 4 && args.length != 5 || !args[1].matches(fileFilter) || !args[2].matches(parentFilter) || !args[3].matches(commentFilter))
-			throw new IllegalArgumentException("Usage: lister DATABASE " + fileFilter + " " + parentFilter + " " + commentFilter + " " + comment);
+			throw new IllegalArgumentException("Usage: lister " + dbname + " " + fileFilter + " " + parentFilter + " " + commentFilter + " " + comment);
 
 		dbname = args[0];
 		fileFilter = args[1];
@@ -34,13 +67,13 @@ public class Lister {//TODO Valid, Obsolete, Missing, With / Without
 			comment = args[4];
 
 		Lister lister = (Lister) SpringUtil.getContext().getBean("lister");
-		lister.list(dbname, fileFilter, parentFilter, commentFilter, comment);
+		lister.list_old(dbname, fileFilter, parentFilter, commentFilter, comment);
 	}
 
 	@Autowired
 	private DatabankDAO	dbdao;
 
-	public void list(String dbname, String fileFilter, String parentFilter, String commentFilter, String comment) throws Exception {
+	public void list_old(String dbname, String fileFilter, String parentFilter, String commentFilter, String comment) throws Exception {
 		Databank db = dbdao.findByExample(new Databank(dbname), "id", "reference", "filelink", "parent", "regex", "crawltype", "entries");
 		if (db == null)
 			new IllegalArgumentException("Databank with name " + dbname + " not found.");

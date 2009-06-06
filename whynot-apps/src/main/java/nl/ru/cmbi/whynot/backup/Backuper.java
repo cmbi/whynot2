bddfcd0de35a91163e11bef478a2d0bb27079cc4
Tim te Beek <ttebeek@bio-prodict.nl>
2009-06-06 16:05:49 +0000

    Completed Backuper

--- whynot-apps/src/main/java/nl/ru/cmbi/whynot/backup/Backuper.java
+++ whynot-apps/src/main/java/nl/ru/cmbi/whynot/backup/Backuper.java
@@ -1,5 +1,7 @@
 package nl.ru.cmbi.whynot.backup;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,9 +11,14 @@ import nl.ru.cmbi.whynot.hibernate.GenericDAO.CommentDAO;
 import nl.ru.cmbi.whynot.model.Annotation;
 import nl.ru.cmbi.whynot.model.Comment;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class Backuper {
 	public static void main(String[] args) throws BeansException, IOException {
 		((Backuper) SpringUtil.getContext().getBean("backuper")).backup();
@@ -20,10 +27,16 @@ public class Backuper {
 	@Autowired
 	private CommentDAO	comdao;
 
-	private void backup() throws IOException {
-		PrintWriter pw = new PrintWriter(new FileWriter(System.currentTimeMillis() + ".backup"));
+	public void backup() throws IOException {
+		File dirBackup = new File("backup/");
+		//Make sure download directory exist
+		if (!dirBackup.isDirectory() && !dirBackup.mkdir())
+			throw new FileNotFoundException(dirBackup.getAbsolutePath());
+
+		File file = new File("backup/" + System.currentTimeMillis() + ".backup");
+		PrintWriter pw = new PrintWriter(new FileWriter(file));
 		for (Comment com : comdao.findAll()) {
-			pw.println(com.getText());
+			pw.println("COMMENT: " + com.getText());
 			for (Annotation ann : com.getAnnotations()) {
 				pw.print(ann.getEntry().getDatabank().getName());
 				pw.print(',');
@@ -31,5 +44,6 @@ public class Backuper {
 			}
 		}
 		pw.close();
+		Logger.getLogger(getClass()).info("Backup complete: " + file.getAbsolutePath());
 	}
 }

857b15256f7e75a7af1946c2ebbaec93b7f229ad
Tim te Beek <ttebeek@bio-prodict.nl>
2009-06-08 07:22:11 +0000

    Added @see comments

--- whynot-ws/src/main/java/test/RNGImpl.java
+++ whynot-ws/src/main/java/test/RNGImpl.java
@@ -6,11 +6,16 @@ import javax.jws.WebService;
 
 @WebService(endpointInterface = "test.RNG")
 public class RNGImpl implements RNG {
+	/**
+	 * @see http://localhost:8080/whynot-ws/webservice/getRandomNumber
+	 */
 	public double getRandomNumber() {
 		return new Random().nextDouble();
 	}
 
-	@Override
+	/**
+	 * @see http://localhost:8080/whynot-ws/webservice/getRandomNumberBetween/min/4/max/12
+	 */
 	public int getRandomNumberBetween(int min, int max) {
 		return new Random().nextInt(max) + min;
 	}

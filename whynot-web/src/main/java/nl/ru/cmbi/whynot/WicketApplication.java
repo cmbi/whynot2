8647653a80a8e9853b4210cc6bb811e2da690f02
Tim te Beek <ttebeek@bio-prodict.nl>
2009-06-09 22:55:06 +0000

    Implemented status page Moved table specific css into general stylesheet

--- whynot-web/src/main/java/nl/ru/cmbi/whynot/WicketApplication.java
+++ whynot-web/src/main/java/nl/ru/cmbi/whynot/WicketApplication.java
@@ -7,6 +7,7 @@ import nl.ru.cmbi.whynot.databank.DatabankPage;
 import nl.ru.cmbi.whynot.error.ErrorPage;
 import nl.ru.cmbi.whynot.home.HomePage;
 import nl.ru.cmbi.whynot.search.SearchResultsPage;
+import nl.ru.cmbi.whynot.status.StatusPage;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
@@ -30,6 +31,7 @@ public class WicketApplication extends WebApplication {
 		mountBookmarkablePage("comments", CommentPage.class);
 		mountBookmarkablePage("databanks", DatabankPage.class);
 		mountBookmarkablePage("entries", DatabankEntriesPage.class);
+		mountBookmarkablePage("status", StatusPage.class);
 		mountBookmarkablePage("search", SearchResultsPage.class);
 	}
 

11b768e8a26557ca70b86e0f877105793427c626
Tim te Beek <ttebeek@bio-prodict.nl>
2009-06-11 16:09:17 +0000

    Share fixed

---
+++ whynot-web/src/main/java/nl/ru/cmbi/whynot/webservice/Whynot.java
@@ -0,0 +1,15 @@
+package nl.ru.cmbi.whynot.webservice;
+
+import java.util.List;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import nl.ru.cmbi.whynot.model.Databank.CollectionType;
+
+@WebService
+public interface Whynot {
+	List<String> getAnnotations(@WebParam(name = "databank") String databank, @WebParam(name = "pdbid") String pdbid);
+
+	List<String> getEntries(@WebParam(name = "databank") String databank, @WebParam(name = "selection") CollectionType selection);
+}

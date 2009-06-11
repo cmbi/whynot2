9de3cbdb0d62b2725061f5b6ecb7373d231a906d
Tim te Beek <ttebeek@bio-prodict.nl>
2009-06-11 17:28:30 +0000

    External mvn is the key

---
+++ whynot-apps/src/main/java/MainClass.java
@@ -0,0 +1,13 @@
+public class MainClass {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Usage:");
+		System.out.println("java -cp dependecy/ -jar whynot-apps.jar nl.ru.cmbi.whynot.util.Installer");
+		System.out.println("java -cp dependecy/ -jar whynot-apps.jar nl.ru.cmbi.whynot.crawl.Crawler");
+		System.out.println("java -cp dependecy/ -jar whynot-apps.jar nl.ru.cmbi.whynot.annotate.Annotater");
+		System.out.println("java -cp dependecy/ -jar whynot-apps.jar nl.ru.cmbi.whynot.list.Lister");
+		System.out.println("java -cp dependecy/ -jar whynot-apps.jar nl.ru.cmbi.whynot.backup.Backuper");
+	}
+}

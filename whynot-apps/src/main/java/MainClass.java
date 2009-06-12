55a181b6faf4aad5e5ba099e75aac857303d39a0
Tim te Beek <ttebeek@bio-prodict.nl>
2009-06-12 09:00:07 +0000

    Cleaned up MainClass

--- whynot-apps/src/main/java/MainClass.java
+++ whynot-apps/src/main/java/MainClass.java
@@ -1,13 +1,14 @@
+import nl.ru.cmbi.whynot.annotate.Annotater;
+import nl.ru.cmbi.whynot.backup.Backuper;
+import nl.ru.cmbi.whynot.crawl.Crawler;
+import nl.ru.cmbi.whynot.list.Lister;
+import nl.ru.cmbi.whynot.util.Install;
+
 public class MainClass {
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		System.out.println("Usage:");
-		System.out.println("java -cp dependecy/ -jar whynot-apps.jar nl.ru.cmbi.whynot.util.Installer");
-		System.out.println("java -cp dependecy/ -jar whynot-apps.jar nl.ru.cmbi.whynot.crawl.Crawler");
-		System.out.println("java -cp dependecy/ -jar whynot-apps.jar nl.ru.cmbi.whynot.annotate.Annotater");
-		System.out.println("java -cp dependecy/ -jar whynot-apps.jar nl.ru.cmbi.whynot.list.Lister");
-		System.out.println("java -cp dependecy/ -jar whynot-apps.jar nl.ru.cmbi.whynot.backup.Backuper");
+		String base = "java -cp dependency/*:whynot-apps-2.0-090612.jar ";
+		for (Class<Object> clazz : new Class[] { Install.class, Crawler.class, Annotater.class, Lister.class, Backuper.class })
+			System.out.println(base + clazz.getCanonicalName());
 	}
 }

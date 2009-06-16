36cfbc2ccc45636170efb63afe9a5042bf5a44ad
Tim te Beek <ttebeek@bio-prodict.nl>
2009-06-16 11:51:01 +0000

    Removed single line functions by moving line into caller

--- whynot-core/src/main/java/nl/ru/cmbi/whynot/hibernate/EntryHibernateDAO.java
+++ whynot-core/src/main/java/nl/ru/cmbi/whynot/hibernate/EntryHibernateDAO.java
@@ -1,6 +1,5 @@
 package nl.ru.cmbi.whynot.hibernate;
 
-import java.util.Collection;
 import java.util.List;
 
 import nl.ru.cmbi.whynot.hibernate.GenericDAO.EntryDAO;
@@ -26,80 +25,48 @@ public class EntryHibernateDAO extends GenericHibernateDAO<Entry, Long> implemen
 		return !findByCriteria(Restrictions.naturalId().set("pdbid", pdbid)).isEmpty();
 	}
 
-	//TODO: Use these to filter collections internally
+	//Filters
 	//Unfortunately these only work with persistent collections and thus can't be chained.
-	//We _could_ add multiple conditions per filter, but that wont help in finding missing
-	//entries, as these can (and often will) not exist if neither file nor annotations exist
+	//We _could_ add more conditions per filter, but that wont help in finding missing entries,
+	//as these can (and often will) not exist if neither file nor annotations exist
 	@SuppressWarnings("unchecked")
-	private List<Entry> withFile(Collection<Entry> collection) {//Present (Valid + Obsolete)
-		return getSession().createFilter(collection, "where this.file is not null").list();
-	}
-
-	@SuppressWarnings("unchecked")
-	private List<Entry> withAnnotation(Collection<Entry> collection) {//Annotated
-		return getSession().createFilter(collection, "where this.annotations is not empty").list();
-	}
-
-	@SuppressWarnings("unchecked")
-	private List<Entry> withParentFile(Collection<Entry> collection) {//Valid + Missing
-		return getSession().createFilter(collection, "where (select par.file from this.databank.parent.entries par where par.pdbid = this.pdbid) is not null").list();
-	}
-
-	@SuppressWarnings("unchecked")
-	private List<Entry> withoutParentFile(Collection<Entry> collection) {//Obsolete
-		return getSession().createFilter(collection, "where (select par.file from this.databank.parent.entries par where par.pdbid = this.pdbid) is null").list();
-	}
-
-	@SuppressWarnings("unchecked")
-	private List<Entry> withFileAndParentFile(Collection<Entry> collection) {//Valid
-		return getSession().createFilter(collection, "where this.file is not null and (select par.file from this.databank.parent.entries par where par.pdbid = this.pdbid) is not null").list();
+	public List<Entry> getPresent(Databank db) {
+		return getSession().createFilter(db.getEntries(), "where this.file is not null").list();
 	}
 
 	@Override
-	public List<Entry> getPresent(Databank db) {
-		System.out.println(db.getEntries().size());//All
-		System.out.println(withFile(db.getEntries()).size()); //Present (Valid + Obsolete)
-		System.out.println(withAnnotation(db.getEntries()).size());//Annotated
-		System.out.println(withParentFile(db.getEntries()).size());//Valid + Missing
-		System.out.println(withoutParentFile(db.getEntries()).size());//Obsolete
-		System.out.println(withFileAndParentFile(db.getEntries()).size());//Valid
-		return null;
+	public long getPresentCount(Databank child) {
+		return getPresent(child).size();
 	}
-
-	//Child file present, parent file present
-	private String	valid	= "from Entry child where file is not null and child.databank = :child_db and (select parent.file from Entry parent where parent.pdbid = child.pdbid and parent.databank = :parent_db) is not null";
 
 	@SuppressWarnings("unchecked")
-	public List<Entry> getValid(Databank child) {
-		Query q = getSession().createQuery(valid + " order by pdbid");
-		q.setParameter("child_db", child);
-		q.setParameter("parent_db", child.getParent());
-		return q.list();
+	public List<Entry> getValid(Databank db) {
+		return getSession().createFilter(db.getEntries(), "where this.file is not null and (select par.file from this.databank.parent.entries par where par.pdbid = this.pdbid) is not null").list();
 	}
 
+	@Override
 	public long getValidCount(Databank child) {
-		Query q = getSession().createQuery("select count(*) " + valid);
-		q.setParameter("child_db", child);
-		q.setParameter("parent_db", child.getParent());
-		return (Long) q.uniqueResult();
+		return getValid(child).size();
 	}
-
-	//Child file present, no parent file
-	private String	obsolete	= "from Entry child where file is not null and child.databank = :child_db and (select parent.file from Entry parent where parent.pdbid = child.pdbid and parent.databank = :parent_db) is null";
 
 	@SuppressWarnings("unchecked")
-	public List<Entry> getObsolete(Databank child) {
-		Query q = getSession().createQuery(obsolete + " order by pdbid");
-		q.setParameter("child_db", child);
-		q.setParameter("parent_db", child.getParent());
-		return q.list();
+	public List<Entry> getObsolete(Databank db) {
+		return getSession().createFilter(db.getEntries(), "where (select par.file from this.databank.parent.entries par where par.pdbid = this.pdbid) is null").list();
 	}
 
+	@Override
 	public long getObsoleteCount(Databank child) {
-		Query q = getSession().createQuery("select count(*) " + obsolete);
-		q.setParameter("child_db", child);
-		q.setParameter("parent_db", child.getParent());
-		return (Long) q.uniqueResult();
+		return getObsolete(child).size();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Entry> getAnnotated(Databank db) {
+		return getSession().createFilter(db.getEntries(), "where this.annotations is not empty").list();
+	}
+
+	@Override
+	public long getAnnotatedCount(Databank child) {
+		return getAnnotated(child).size();
 	}
 
 	//Parent file present, no child file
@@ -135,22 +102,6 @@ public class EntryHibernateDAO extends GenericHibernateDAO<Entry, Long> implemen
 		Query q = getSession().createQuery("select count(*) " + unannotated);
 		q.setParameter("child_db", child);
 		q.setParameter("parent_db", child.getParent());
-		return (Long) q.uniqueResult();
-	}
-
-	//No child file (annotations implicitly present or there wouldnt be a child)
-	private String	annotated	= "from Entry child where child.file is null and child.databank = :child_db";
-
-	@SuppressWarnings("unchecked")
-	public List<Entry> getAnnotated(Databank child) {
-		Query q = getSession().createQuery(annotated + " order by pdbid");
-		q.setParameter("child_db", child);
-		return q.list();
-	}
-
-	public long getAnnotatedCount(Databank child) {
-		Query q = getSession().createQuery("select count(*) " + annotated);
-		q.setParameter("child_db", child);
 		return (Long) q.uniqueResult();
 	}
 

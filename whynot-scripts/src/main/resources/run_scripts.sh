de23a8fe6063785e2dec09c0ef58e1e32ff8f0ee
Tim te Beek <ttebeek@bio-prodict.nl>
2009-08-11 11:59:51 +0000

    added line to remove empty generated comment files

--- whynot-scripts/src/main/resources/run_scripts.sh
+++ whynot-scripts/src/main/resources/run_scripts.sh
@@ -19,3 +19,5 @@ java -cp dependency/*:whynot-apps.jar nl.ru.cmbi.whynot.list.Lister DSSP UNANNOT
 
 java -cp dependency/*:whynot-apps.jar nl.ru.cmbi.whynot.list.Lister DSSP UNANNOTATED\
 	| java scripts.GetDSSPErrors > comment/DSSPCMBI_Errors_$(date +%y%m%d).txt;
+
+find comment/ -size 0 -print0|xargs -0 rm;

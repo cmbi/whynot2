73f69d8cab330c513bd1d474c984be1a3ff159b4
Tim te Beek <ttebeek@bio-prodict.nl>
2009-09-04 09:58:44 +0000

    Disable custom errorpages for AccessDenied & PageExpired

--- whynot-web/src/main/java/nl/ru/cmbi/whynot/WicketApplication.java
+++ whynot-web/src/main/java/nl/ru/cmbi/whynot/WicketApplication.java
@@ -5,13 +5,18 @@ import nl.ru.cmbi.whynot.comment.CommentPage;
 import nl.ru.cmbi.whynot.databank.DatabankPage;
 import nl.ru.cmbi.whynot.databank.ListInitializer;
 import nl.ru.cmbi.whynot.entries.EntriesPage;
-import nl.ru.cmbi.whynot.error.ErrorPage;
+import nl.ru.cmbi.whynot.error.MyExceptionErrorPage;
 import nl.ru.cmbi.whynot.home.HomePage;
 import nl.ru.cmbi.whynot.search.ResultsPage;
 import nl.ru.cmbi.whynot.statistics.StatisticsPage;
 
+import org.apache.wicket.Request;
+import org.apache.wicket.RequestCycle;
+import org.apache.wicket.Response;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.protocol.http.WebRequest;
+import org.apache.wicket.protocol.http.WebResponse;
 import org.apache.wicket.settings.IExceptionSettings;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
@@ -30,9 +35,9 @@ public class WicketApplication extends WebApplication {
 		//getRequestLoggerSettings().setRequestLoggerEnabled(true);
 
 		//ErrorPages
-		getApplicationSettings().setAccessDeniedPage(ErrorPage.class);
-		getApplicationSettings().setPageExpiredErrorPage(ErrorPage.class);
-		getApplicationSettings().setInternalErrorPage(ErrorPage.class);
+		//getApplicationSettings().setAccessDeniedPage(MyExceptionErrorPage.class);
+		//getApplicationSettings().setPageExpiredErrorPage(MyExceptionErrorPage.class);
+		getApplicationSettings().setInternalErrorPage(MyExceptionErrorPage.class);
 		getExceptionSettings().setUnexpectedExceptionDisplay(IExceptionSettings.SHOW_INTERNAL_ERROR_PAGE);
 
 		//Pretty URLs
@@ -40,12 +45,17 @@ public class WicketApplication extends WebApplication {
 		mountBookmarkablePage("comments", CommentPage.class);
 		mountBookmarkablePage("databanks", DatabankPage.class);
 		mountBookmarkablePage("entries", EntriesPage.class);
-		mountBookmarkablePage("error", ErrorPage.class);
+		mountBookmarkablePage("error", MyExceptionErrorPage.class);
 		mountBookmarkablePage("search", ResultsPage.class);
 		mountBookmarkablePage("statistics", StatisticsPage.class);
 
 		//Register export lists as shared resources
 		getSharedResources().putClassAlias(ListInitializer.class, "list");
 		new ListInitializer().init(this);
+	}
+
+	@Override
+	public RequestCycle newRequestCycle(Request request, Response response) {
+		return new MyRequestCycle(this, (WebRequest) request, (WebResponse) response);
 	}
 }

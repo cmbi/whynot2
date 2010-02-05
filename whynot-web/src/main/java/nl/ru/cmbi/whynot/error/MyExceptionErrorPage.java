e02a4d92f6de19636bd423f5ebf7f4c0f6f17ae3
Tim te Beek <ttebeek@bio-prodict.nl>
2010-02-05 21:45:09 +0000

    Add h3 page

--- whynot-web/src/main/java/nl/ru/cmbi/whynot/error/MyExceptionErrorPage.java
+++ whynot-web/src/main/java/nl/ru/cmbi/whynot/error/MyExceptionErrorPage.java
@@ -17,6 +17,8 @@ public class MyExceptionErrorPage extends WebPage {
 	 */
 	public MyExceptionErrorPage(Page page, RuntimeException e) {
 		add(new FeedbackPanelWrapper("feedback"));
+		
+		add(new Label("page", page.toString()));
 
 		StringWriter s = new StringWriter();
 		e.printStackTrace(new PrintWriter(s));

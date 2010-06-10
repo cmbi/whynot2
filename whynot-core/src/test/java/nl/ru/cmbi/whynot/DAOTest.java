ad2924c06fa94bad79c2d9019600111304b64cff
Tim te Beek <ttebeek@bio-prodict.nl>
2010-06-10 08:41:56 +0000

    Improved testing

--- whynot-core/src/test/java/nl/ru/cmbi/whynot/DAOTest.java
+++ whynot-core/src/test/java/nl/ru/cmbi/whynot/DAOTest.java
@@ -1,13 +1,10 @@
 package nl.ru.cmbi.whynot;
 
-import nl.ru.cmbi.whynot.hibernate.GenericDAO.AnnotationDAO;
-import nl.ru.cmbi.whynot.hibernate.GenericDAO.CommentDAO;
 import nl.ru.cmbi.whynot.hibernate.GenericDAO.DatabankDAO;
 import nl.ru.cmbi.whynot.hibernate.GenericDAO.EntryDAO;
-import nl.ru.cmbi.whynot.hibernate.GenericDAO.FileDAO;
 import nl.ru.cmbi.whynot.model.Databank;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,34 +15,52 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/spring.xml" })
 @Transactional
-@SuppressWarnings("unused")
 public class DAOTest {
 	@Autowired
-	private AnnotationDAO	anndao;
+	private DatabankDAO	dbdao;
 	@Autowired
-	private CommentDAO		comdao;
-	@Autowired
-	private DatabankDAO		dbdao;
-	@Autowired
-	private EntryDAO		entdao;
-	@Autowired
-	private FileDAO			filedao;
+	private EntryDAO	entdao;
+
+	private Databank	dssp;
+
+	@Before
+	public void getDatabank() {
+		dssp = dbdao.findByName("DSSP");
+	}
 
 	@Test
-	@Ignore
+	public void getPresent() {
+		int count = entdao.getPresent(dssp).size();
+		System.out.println(count);
+	}
+
+	@Test
 	public void getValid() {
-		Databank dssp = dbdao.findByName("DSSP");
-		//int count = entdao.countValid(dssp);
 		int count = entdao.getValid(dssp).size();
 		System.out.println(count);
 	}
 
 	@Test
-	@Ignore
-	public void getPresent() {
-		Databank dssp = dbdao.findByName("DSSP");
-		//int count = entdao.countPresent(dssp);
-		int count = entdao.getPresent(dssp).size();
+	public void getObsolete() {
+		int count = entdao.getObsolete(dssp).size();
+		System.out.println(count);
+	}
+
+	@Test
+	public void getMissing() {
+		int count = entdao.getMissing(dssp).size();
+		System.out.println(count);
+	}
+
+	@Test
+	public void getAnnotated() {
+		int count = entdao.getAnnotated(dssp).size();
+		System.out.println(count);
+	}
+
+	@Test
+	public void getUnannotated() {
+		int count = entdao.getUnannotated(dssp).size();
 		System.out.println(count);
 	}
 }

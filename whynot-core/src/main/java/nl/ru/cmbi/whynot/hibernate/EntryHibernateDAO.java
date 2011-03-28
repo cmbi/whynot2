875fe3c737dad17c70497eb936277f4da84258f8
Tim te Beek <ttebeek@bio-prodict.nl>
2011-03-28 08:32:29 +0000

    Add FIXME after incoming request from Robbie

--- whynot-core/src/main/java/nl/ru/cmbi/whynot/hibernate/EntryHibernateDAO.java
+++ whynot-core/src/main/java/nl/ru/cmbi/whynot/hibernate/EntryHibernateDAO.java
@@ -16,13 +16,13 @@ import nl.ru.cmbi.whynot.model.Entry;
 @Service
 public class EntryHibernateDAO extends GenericHibernateDAO<Entry> implements EntryDAO {
 	@Override
-	public Entry findByDatabankAndPdbid(Databank databank, String pdbid) {
+	public Entry findByDatabankAndPdbid(final Databank databank, final String pdbid) {
 		Criteria crit = createCriteria(Restrictions.naturalId().set("databank", databank).set("pdbid", pdbid));
 		return (Entry) crit.uniqueResult();
 	}
 
 	@Override
-	public boolean contains(String pdbid) {
+	public boolean contains(final String pdbid) {
 		Criteria crit = createCriteria(Restrictions.naturalId().set("pdbid", pdbid));
 		crit.setProjection(Projections.rowCount());
 		return 0 < (Integer) crit.uniqueResult();
@@ -31,12 +31,12 @@ public class EntryHibernateDAO extends GenericHibernateDAO<Entry> implements Ent
 	//Present
 	@Override
 	@SuppressWarnings("unchecked")
-	public List<Entry> getPresent(Databank db) {
+	public List<Entry> getPresent(final Databank db) {
 		return getSession().createFilter(db.getEntries(), "where this.file is not null").list();
 	}
 
 	@Override
-	public int countPresent(Databank db) {
+	public int countPresent(final Databank db) {
 		Criteria crit = createCriteria(Restrictions.eq("databank", db), Restrictions.isNotNull("file"));
 		return (Integer) crit.setProjection(Projections.rowCount()).uniqueResult();
 	}
@@ -44,12 +44,12 @@ public class EntryHibernateDAO extends GenericHibernateDAO<Entry> implements Ent
 	//Valid
 	@Override
 	@SuppressWarnings("unchecked")
-	public List<Entry> getValid(Databank db) {
+	public List<Entry> getValid(final Databank db) {
 		return getSession().createFilter(db.getEntries(), "where this.file is not null and (select par.file from this.databank.parent.entries par where par.pdbid = this.pdbid) is not null").list();
 	}
 
 	@Override
-	public int countValid(Databank db) {
+	public int countValid(final Databank db) {
 		Criteria crit = createCriteria(Restrictions.eq("databank", db), Restrictions.isNotNull("file"));
 		crit.add(Restrictions.sqlRestriction("(select parent.file_id from Entry parent where parent.pdbid = {alias}.pdbid and parent.databank_id = ?) is not null", db.getParent().getId(), Hibernate.LONG));
 		return (Integer) crit.setProjection(Projections.rowCount()).uniqueResult();
@@ -58,12 +58,12 @@ public class EntryHibernateDAO extends GenericHibernateDAO<Entry> implements Ent
 	//Obsolete
 	@Override
 	@SuppressWarnings("unchecked")
-	public List<Entry> getObsolete(Databank db) {
+	public List<Entry> getObsolete(final Databank db) {
 		return getSession().createFilter(db.getEntries(), "where (select par.file from this.databank.parent.entries par where par.pdbid = this.pdbid) is null").list();
 	}
 
 	@Override
-	public int countObsolete(Databank db) {
+	public int countObsolete(final Databank db) {
 		Criteria crit = createCriteria(Restrictions.eq("databank", db));
 		crit.add(Restrictions.sqlRestriction("(select parent.file_id from Entry parent where parent.pdbid = {alias}.pdbid and parent.databank_id = ?) is null", db.getParent().getId(), Hibernate.LONG));
 		return (Integer) crit.setProjection(Projections.rowCount()).uniqueResult();
@@ -72,12 +72,12 @@ public class EntryHibernateDAO extends GenericHibernateDAO<Entry> implements Ent
 	//Annotated
 	@Override
 	@SuppressWarnings("unchecked")
-	public List<Entry> getAnnotated(Databank db) {
+	public List<Entry> getAnnotated(final Databank db) {
 		return getSession().createFilter(db.getEntries(), "where this.annotations is not empty").list();
 	}
 
 	@Override
-	public int countAnnotated(Databank db) {
+	public int countAnnotated(final Databank db) {
 		Criteria crit = createCriteria(Restrictions.eq("databank", db), Restrictions.isNotEmpty("annotations"));
 		return (Integer) crit.setProjection(Projections.rowCount()).uniqueResult();
 	}
@@ -85,14 +85,15 @@ public class EntryHibernateDAO extends GenericHibernateDAO<Entry> implements Ent
 	//Missing
 	@Override
 	@SuppressWarnings("unchecked")
-	public List<Entry> getMissing(Databank child) {
+	public List<Entry> getMissing(final Databank child) {
+		//FIXME Obsolete parent entries should (maybe) not result in missing child entries
 		Criteria crit = createCriteria(Restrictions.eq("databank", child.getParent()), Restrictions.isNotNull("file"));
 		crit.add(Restrictions.sqlRestriction("(select child.file_id from Entry child where {alias}.pdbid = child.pdbid and child.databank_id = ?) is null", child.getId(), Hibernate.LONG));
 		return crit.addOrder(Order.asc("pdbid")).list();
 	}
 
 	@Override
-	public int countMissing(Databank child) {
+	public int countMissing(final Databank child) {
 		Criteria crit = createCriteria(Restrictions.eq("databank", child.getParent()), Restrictions.isNotNull("file"));
 		crit.add(Restrictions.sqlRestriction("(select child.file_id from Entry child where {alias}.pdbid = child.pdbid and child.databank_id = ?) is null", child.getId(), Hibernate.LONG));
 		return (Integer) crit.setProjection(Projections.rowCount()).uniqueResult();
@@ -101,14 +102,14 @@ public class EntryHibernateDAO extends GenericHibernateDAO<Entry> implements Ent
 	//Unannotated
 	@Override
 	@SuppressWarnings("unchecked")
-	public List<Entry> getUnannotated(Databank child) {
+	public List<Entry> getUnannotated(final Databank child) {
 		Criteria crit = createCriteria(Restrictions.eq("databank", child.getParent()), Restrictions.isNotNull("file"));
 		crit.add(Restrictions.sqlRestriction("(select child from Entry child where {alias}.pdbid = child.pdbid and child.databank_id = ?) is null", child.getId(), Hibernate.LONG));
 		return crit.addOrder(Order.asc("pdbid")).list();
 	}
 
 	@Override
-	public int counUnannotated(Databank child) {
+	public int counUnannotated(final Databank child) {
 		Criteria crit = createCriteria(Restrictions.eq("databank", child.getParent()), Restrictions.isNotNull("file"));
 		crit.add(Restrictions.sqlRestriction("(select child from Entry child where {alias}.pdbid = child.pdbid and child.databank_id = ?) is null", child.getId(), Hibernate.LONG));
 		return (Integer) crit.setProjection(Projections.rowCount()).uniqueResult();

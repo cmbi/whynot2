a24136b3703c60106a91e892ee60f80bc1d32374
Tim te Beek <ttebeek@bio-prodict.nl>
2012-07-06 11:32:59 +0000

    Show databanks page in order of hierarchy Show full file path on Statistics page

--- whynot-core/src/main/java/nl/ru/cmbi/whynot/hibernate/DatabankHibernateDAO.java
+++ whynot-core/src/main/java/nl/ru/cmbi/whynot/hibernate/DatabankHibernateDAO.java
@@ -1,26 +1,45 @@
 package nl.ru.cmbi.whynot.hibernate;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import nl.ru.cmbi.whynot.hibernate.GenericDAO.DatabankDAO;
+import nl.ru.cmbi.whynot.model.Databank;
 
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import nl.ru.cmbi.whynot.hibernate.GenericDAO.DatabankDAO;
-import nl.ru.cmbi.whynot.model.Databank;
-
 @Service
 public class DatabankHibernateDAO extends GenericHibernateDAO<Databank> implements DatabankDAO {
 	@Override
-	@SuppressWarnings("unchecked")
-	@Transactional
-	public List<Databank> getAll() {
-		return createCriteria().addOrder(Order.asc("name")).list();
+	public Databank findByName(final String name) {
+		return (Databank) createCriteria(Restrictions.naturalId().set("name", name)).uniqueResult();
 	}
 
 	@Override
-	public Databank findByName(String name) {
-		return (Databank) createCriteria(Restrictions.naturalId().set("name", name)).uniqueResult();
+	@Transactional
+	public List<Databank> getAll() {
+		// Get root databank PDB
+		Databank pdb = findByName("PDB");
+
+		// Get all databanks
+		@SuppressWarnings("unchecked")
+		List<Databank> allDatabanks = createCriteria().addOrder(Order.asc("name")).list();
+
+		// Return databanks in hierachical order
+		return getDatabanksInTreeOrder(pdb, allDatabanks);
+	}
+
+	private List<Databank> getDatabanksInTreeOrder(final Databank rootdb, final List<Databank> allDatabanks) {
+		List<Databank> children = new ArrayList<Databank>();
+		// Add root node
+		children.add(rootdb);
+		for (Databank child : allDatabanks)
+			if (!rootdb.equals(child) && child.getParent().equals(rootdb))
+				// Recursively add child itself and grandchildren
+				children.addAll(getDatabanksInTreeOrder(child, allDatabanks));
+		return children;
 	}
 }
--- whynot-web/src/main/java/nl/ru/cmbi/whynot/statistics/StatisticsPage.java
+++ whynot-web/src/main/java/nl/ru/cmbi/whynot/statistics/StatisticsPage.java
@@ -3,11 +3,6 @@ package nl.ru.cmbi.whynot.statistics;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-
 import nl.ru.cmbi.whynot.hibernate.GenericDAO.AnnotationDAO;
 import nl.ru.cmbi.whynot.hibernate.GenericDAO.CommentDAO;
 import nl.ru.cmbi.whynot.hibernate.GenericDAO.DatabankDAO;
@@ -17,30 +12,37 @@ import nl.ru.cmbi.whynot.home.HomePage;
 import nl.ru.cmbi.whynot.model.Annotation;
 import nl.ru.cmbi.whynot.model.File;
 
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
 public class StatisticsPage extends HomePage {
 	@SpringBean
-	private AnnotationDAO		anndao;
+	private AnnotationDAO	anndao;
 	@SpringBean
-	private CommentDAO			comdao;
+	private CommentDAO		comdao;
 	@SpringBean
-	private DatabankDAO			dbdao;
+	private DatabankDAO		dbdao;
 	@SpringBean
-	private EntryDAO			entdao;
+	private EntryDAO		entdao;
 	@SpringBean
-	private FileDAO				filedao;
+	private FileDAO			filedao;
 
-	private SimpleDateFormat	sdf	= new SimpleDateFormat("dd/MM/yyyy HH:mm");
+	SimpleDateFormat		sdf	= new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
 	public StatisticsPage() {
+		// Counts
 		add(new Label("annotations", "" + anndao.countAll()));
 		add(new Label("comments", "" + comdao.countAll()));
 		add(new Label("databanks", "" + dbdao.countAll()));
 		add(new Label("entries", "" + entdao.countAll()));
 		add(new Label("files", "" + filedao.countAll()));
 
+		// Most recent annotations
 		ListView<Annotation> lva = new ListView<Annotation>("annotation", anndao.getRecent()) {
 			@Override
-			protected void populateItem(ListItem<Annotation> item) {
+			protected void populateItem(final ListItem<Annotation> item) {
 				Annotation ann = item.getModelObject();
 				item.add(new Label("comment", ann.getComment().getText()));
 				item.add(new Label("databank", ann.getEntry().getDatabank().getName()));
@@ -50,12 +52,12 @@ public class StatisticsPage extends HomePage {
 		};
 		add(lva);
 
+		// Most recent files
 		ListView<File> lvf = new ListView<File>("file", filedao.getRecent()) {
 			@Override
-			protected void populateItem(ListItem<File> item) {
+			protected void populateItem(final ListItem<File> item) {
 				File file = item.getModelObject();
 				String path = file.getPath();
-				path = path.substring(path.lastIndexOf('/') + 1);
 				item.add(new Label("path", path));
 				item.add(new Label("timestamp", sdf.format(new Date(file.getTimestamp()))));
 			}

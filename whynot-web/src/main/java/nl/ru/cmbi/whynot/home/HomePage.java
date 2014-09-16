ddca60657212edda4804ed804414b01a8f0bd3ce
Coos Baakman <cbaakman@gmail.com>
2014-09-16 12:14:27 +0200

    Adjusted hierarchy listing to whatif lists and scenes

--- whynot-web/src/main/java/nl/ru/cmbi/whynot/home/HomePage.java
+++ whynot-web/src/main/java/nl/ru/cmbi/whynot/home/HomePage.java
@@ -1,12 +1,15 @@
 package nl.ru.cmbi.whynot.home;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import nl.ru.cmbi.whynot.databank.DatabankPage;
 import nl.ru.cmbi.whynot.feedback.FeedbackPanelWrapper;
 import nl.ru.cmbi.whynot.hibernate.GenericDAO.DatabankDAO;
 import nl.ru.cmbi.whynot.model.Databank;
 import nl.ru.cmbi.whynot.search.SearchPanel;
+import nl.ru.cmbi.whynot.util.Utils;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
@@ -16,8 +19,13 @@ import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HomePage extends WebPage {
+	
+	Logger log = LoggerFactory.getLogger(HomePage.class);
+	
 	@SpringBean
 	protected DatabankDAO	databankdao;
 
@@ -30,11 +38,14 @@ public class HomePage extends WebPage {
 	public class DatabankHierarchyFragment extends Fragment {
 		public DatabankHierarchyFragment(final String id, final Databank db, final List<Databank> list) {
 			super(id, "hierarchyfragment", HomePage.this, new Model<Databank>(db));
+			
+			String name=Utils.hierarchyName(db);
+
 			// Link
 			PageParameters pp = new PageParameters();
 			pp.add("name", db.getName());
 			BookmarkablePageLink<WebPage> bpl = new BookmarkablePageLink<WebPage>("databank", DatabankPage.class, pp);
-			add(bpl.add(new Label("name", db.getName())));
+			add(bpl.add(new Label("name", name)));
 
 			// Children
 			RepeatingView children = new RepeatingView("children");

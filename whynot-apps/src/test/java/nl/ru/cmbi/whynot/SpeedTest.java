f376a442a3ea408db4d2781478ef19773bec6cc1
Tim te Beek <ttebeek@bio-prodict.nl>
2015-01-28 11:14:31 +0100

    Ignore SpeedTest aswhynot-testing database is not available everywhere

--- whynot-apps/src/test/java/nl/ru/cmbi/whynot/SpeedTest.java
+++ whynot-apps/src/test/java/nl/ru/cmbi/whynot/SpeedTest.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ import nl.ru.cmbi.whynot.annotate.Annotater;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = WhynotApplication.class)
 @Slf4j
+@Ignore("whynot-testing database is not available in all run/test environments")
 public class SpeedTest {
 	private static final String	WI_OPTIMIZED	= "src/test/resources/wi.optimized.txt";
 
@@ -24,7 +26,7 @@ public class SpeedTest {
 	private Annotater	annotator;
 
 	@Test(timeout = 180 * 1000)
-	// Initial run after update to Spring Boot took 240 seconds; Any further changes may not add to this time
+	// Initial run after update to SpringBoot took 240 seconds; Any further changes may not add to this time
 	public void annotate() throws Exception {
 		log.info("Start");
 		Annotater.comment(annotator, new File(WI_OPTIMIZED));

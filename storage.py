e959cfd73ae43b3694a3ad8018135d1e3bb434b2
Coos Baakman <cbaakman@umcn.nl>
2015-07-28 16:16:57 +0200

    identation type changed

--- storage.py
+++ storage.py
@@ -7,101 +7,108 @@ _log = logging.getLogger(__name__)
 
 class Storage(object):
 
-	def __init__(self, uri=None, db_name=None):
-		self._db = None
-		self._db_name = db_name
-		self._client = None
-		self._uri = uri
+    def __init__(self, uri=None, db_name=None):
+        self._db = None
+        self._db_name = db_name
+        self._client = None
+        self._uri = uri
 
-		if self._uri is not None and self._db_name is not None:
-			self.connect()
-			assert self._db is not None
+        if self._uri is not None and self._db_name is not None:
+            self.connect()
+            assert self._db is not None
 
-	@property
-	def db(self):
-		if self._db is None:
-			self.connect()
-			assert self._db is not None
-		return self._db
+    @property
+    def db(self):
+        if self._db is None:
+            self.connect()
+            assert self._db is not None
+        return self._db
 
-	@property
-	def db_name(self):
-		return self._db_name
+    @property
+    def db_name(self):
+        return self._db_name
 
-	@db_name.setter
-	def db_name(self, db_name):
-		self._db_name = db_name
+    @db_name.setter
+    def db_name(self, db_name):
+        self._db_name = db_name
 
-	@property
-	def uri(self):
-		return self._uri
+    @property
+    def uri(self):
+        return self._uri
 
-	@uri.setter
-	def uri(self, uri):
-		self._uri = uri
+    @uri.setter
+    def uri(self, uri):
+        self._uri = uri
 
-	def connect(self):
-		if self.uri is None or self._db_name is None:
-			raise Exception("Storage hasn't been configured")
+    def connect(self):
+        if self.uri is None or self._db_name is None:
+            raise Exception("Storage hasn't been configured")
 
-		_log.info("Connecting to '{}'".format(self.uri))
-		self._client = MongoClient(self._uri)
-		assert self._client is not None
-		self._db = self._client[self._db_name]
-		assert self._db is not None
+        _log.info("Connecting to '{}'".format(self.uri))
+        self._client = MongoClient(self._uri)
+        assert self._client is not None
+        self._db = self._client[self._db_name]
+        assert self._db is not None
 
-	def insert(self, collection, documents):
-		if self._db is None:
-			raise Exception("Not connected to storage. Did you call connect()?")
+    def insert(self, collection, documents):
+        if self._db is None:
+            raise Exception("Not connected to storage. Did you call connect()?")
 
-		if len(documents) == 0:
-			raise ValueError("Document list is empty. Nothing to insert.")
+        if len(documents) == 0:
+            raise ValueError("Document list is empty. Nothing to insert.")
 
-		_log.info("Inserting documents into '{}'".format(collection))
-		return self._db[collection].insert(documents)
+        _log.info("Inserting documents into '{}'".format(collection))
+        return self._db[collection].insert(documents)
 
-	def update(self, collection, selector, options):
-		if self._db is None:
-			raise Exception("Not connected to storage. Did you call connect()?")
+    def update(self, collection, selector, options):
+        if self._db is None:
+            raise Exception("Not connected to storage. Did you call connect()?")
 
-		_log.info("Updating document in '{}'".format(collection))
-		return self._db[collection].update(selector, options)
+        _log.info("Updating document in '{}'".format(collection))
+        return self._db[collection].update(selector, options)
 
-	def remove(self, collection, spec_or_id=None):
-		if self._db is None:
-			raise Exception("Not connected to storage. Did you call connect()?")
+    def remove(self, collection, spec_or_id=None):
+        if self._db is None:
+            raise Exception("Not connected to storage. Did you call connect()?")
 
-		_log.info("Removing documents from '{}'".format(collection))
-		return self._db[collection].remove(spec_or_id)
+        _log.info("Removing documents from '{}'".format(collection))
+        return self._db[collection].remove(spec_or_id)
 
-	def aggregate(self, collection, stages):
-		if self._db is None:
-			raise Exception("Not connected to storage. Did you call connect()?")
+    def aggregate(self, collection, stages):
+        if self._db is None:
+            raise Exception("Not connected to storage. Did you call connect()?")
 
-		_log.info("Aggregating documents in '{}'".format(collection))
-		cursor = self._db[collection].aggregate(stages)
-		return [d for d in cursor['result']]
+        _log.info("Aggregating documents in '{}'".format(collection))
+        cursor = self._db[collection].aggregate(stages)
+        return [d for d in cursor['result']]
 
-	def find(self, collection, selector, projection=None):
-		if self._db is None:
-			raise Exception("Not connected to storage. Did you call connect()?")
+    def count(self, collection, selector):
+        if self._db is None:
+            raise Exception("Not connected to storage. Did you call connect()?")
 
-		_log.info("Querying documents in '{}'".format(collection))
-		cursor = self._db[collection].find(selector,projection)
-		return [d for d in cursor]
+        _log.info("Counting documents in '{}'".format(collection))
+        return self._db[collection].count(selector)
 
-	def find_one(self, collection, selector, projection=None):
-		if self._db is None:
-			raise Exception("Not connected to storage. Did you call connect()?")
+    def find(self, collection, selector, projection=None):
+        if self._db is None:
+            raise Exception("Not connected to storage. Did you call connect()?")
 
-		_log.info("Querying single document in '{}'".format(collection))
-		return self._db[collection].find_one(selector,projection)
+        _log.info("Querying documents in '{}'".format(collection))
+        cursor = self._db[collection].find(selector,projection)
+        return [d for d in cursor]
 
-	def create_index(self, collection, selector):
-		if self._db is None:
-			raise Exception("Not connected to storage. Did you call connect()?")
-			
-		_log.info("Creating index in '{}'".format(collection))
-		return self._db[collection].create_index(selector)
+    def find_one(self, collection, selector, projection=None):
+        if self._db is None:
+            raise Exception("Not connected to storage. Did you call connect()?")
 
-storage = Storage(uri='mongodb://localhost', db_name='whynot',)
+        _log.info("Querying single document in '{}'".format(collection))
+        return self._db[collection].find_one(selector,projection)
+
+    def create_index(self, collection, selector):
+        if self._db is None:
+            raise Exception("Not connected to storage. Did you call connect()?")
+
+        _log.info("Creating index in '{}'".format(collection))
+        return self._db[collection].create_index(selector)
+
+storage = Storage(uri='mongodb://cmbi4.cmbi.ru.nl', db_name='whynot',)

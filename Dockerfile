bedd3fc3e1f5be7d2a7972c39d6b495ab49d3829
Jon Black <jon_black@mm.st>
2016-10-18 14:44:06 +0200

    Update docker configuration

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,6 @@
-FROM python:2.7
+FROM python:2.7-onbuild
 
-RUN mkdir -p /app
-WORKDIR /app
-
-COPY . /app
-RUN pip install -r requirements
+WORKDIR /usr/src/app
 
 EXPOSE 15000
-ENV WHYNOT_SETTINGS /app/dev_settings.py
-
 CMD ["gunicorn", "-k", "gevent", "-b", "0.0.0.0:15000", "whynot_web.application:app"]
--- prd_settings.py
+++ prd_settings.py
@@ -1,12 +1,12 @@
-DEBUG = True
-SECRET_KEY = 'development_key'
+DEBUG = False
+SECRET_KEY = 'de*ob)98l0mauplafyw+1vs-*t+dvntz2#505**28o+j2#7p(v'
 
 # Debug toolbar
 DEBUG_TB_ENABLED = False
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 # Email logging settings
-MAIL_SERVER = "smpt.umcn.nl"
+MAIL_SERVER = "smtp.umcn.nl"
 MAIL_SMTP_PORT = 25
 MAIL_FROM = "whynot@cmbi.umcn.nl"
 MAIL_TO = ["Coos.Baakman@radboudumc.nl", "Jon.Black@radboudumc.nl"]

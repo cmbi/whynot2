8e501287e3b1ff5bf0e09e421e2f3bf48687599b
Jon Black <jon_black@mm.st>
2016-11-16 14:01:10 +0100

    Add crawl task test

---
+++ tests/unit/test_tasks.py
@@ -0,0 +1,32 @@
+import time
+
+from mock import call, Mock, patch
+
+from whynot.default_settings import DATABANKS
+from whynot.tasks import crawl
+
+
+@patch('whynot.tasks.storage')
+def test_crawl(mock_storage):
+    entries = [
+        {
+            'pdb_id': '1crn', 'file_path': '/srv/data/pdb/flat/pdb1crn.ent',
+            'mtime': time.time(), 'comment': None,
+        },
+        {
+            'pdb_id': '2ltw', 'file_path': '/srv/data/pdb/flat/pdb2ltw.ent',
+            'mtime': time.time(), 'comment': None,
+        },
+    ]
+    mock_dir_crawler = Mock()
+    mock_dir_crawler.crawl.return_value = entries
+    mock_dir_crawler.__name__ = 'MockDirCrawler'
+    databank = DATABANKS[1]
+    databank['crawler'] = mock_dir_crawler
+
+    crawl(databank)
+
+    mock_storage.db.update_one.assert_has_calls([
+        call({ 'databank_name': 'pdb', 'pdb_id': '1crn' }, entries[0], upsert=True),
+        call({ 'databank_name': 'pdb', 'pdb_id': '2ltw' }, entries[1], upsert=True),
+    ])

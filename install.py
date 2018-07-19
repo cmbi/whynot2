cda88a38a5abaaebf851f4558e4395bc6abf0322
Coos Baakman <c.baakman@radboudumc.nl.com>
2018-07-19 11:51:30 +0200

    replaced cmbi.ru.nl by cmbi.umcn.nl

--- install.py
+++ install.py
@@ -41,20 +41,20 @@ def create_databanks():
     docs.append(_create_databank('PDB','http://www.wwpdb.org/',
         'ftp://ftp.wwpdb.org/pub/pdb/data/structures/divided/pdb/${PART}/pdb${PDBID}.ent.gz',
         re.compile(r'.*/pdb([\w]{4})\.ent(\.gz)?'),FILE,'MMCIF'))
-    docs.append(_create_databank('BDB','http://www.cmbi.ru.nl/bdb/',
-        'ftp://ftp.cmbi.ru.nl/pub/molbio/data/bdb/${PART}/${PDBID}/${PDBID}.bdb',
+    docs.append(_create_databank('BDB','http://www.cmbi.umcn.nl/bdb/',
+        'ftp://ftp.cmbi.umcn.nl/pub/molbio/data/bdb/${PART}/${PDBID}/${PDBID}.bdb',
         re.compile(r'.*/([\w]{4})\.bdb'),FILE,'PDB'))
-    docs.append(_create_databank('DSSP','http://swift.cmbi.ru.nl/gv/dssp/',
-        'ftp://ftp.cmbi.ru.nl/pub/molbio/data/dssp/${PDBID}.dssp',
+    docs.append(_create_databank('DSSP','http://swift.cmbi.umcn.nl/gv/dssp/',
+        'ftp://ftp.cmbi.umcn.nl/pub/molbio/data/dssp/${PDBID}.dssp',
         re.compile(r'.*/([\w]{4})\.dssp'),FILE,'MMCIF'))
-    docs.append(_create_databank('HSSP','http://swift.cmbi.ru.nl/gv/hssp/',
-        'ftp://ftp.cmbi.ru.nl/pub/molbio/data/hssp/${PDBID}.hssp.bz2',
+    docs.append(_create_databank('HSSP','http://swift.cmbi.umcn.nl/gv/hssp/',
+        'ftp://ftp.cmbi.umcn.nl/pub/molbio/data/hssp/${PDBID}.hssp.bz2',
         re.compile(r'.*/([\w]{4})\.hssp.bz2'),FILE,'DSSP'))
-    docs.append(_create_databank('PDBFINDER','http://swift.cmbi.ru.nl/gv/pdbfinder/',
-        'ftp://ftp.cmbi.ru.nl/pub/molbio/data/pdbfinder/PDBFIND.TXT.gz',
+    docs.append(_create_databank('PDBFINDER','http://swift.cmbi.umcn.nl/gv/pdbfinder/',
+        'ftp://ftp.cmbi.umcn.nl/pub/molbio/data/pdbfinder/PDBFIND.TXT.gz',
         re.compile(r'ID           : ([\w]{4})'),LINE,'HSSP'))
-    docs.append(_create_databank('PDBFINDER2','http://swift.cmbi.ru.nl/gv/pdbfinder/',
-        'ftp://ftp.cmbi.ru.nl/pub/molbio/data/pdbfinder2/PDBFIND2.TXT.gz',
+    docs.append(_create_databank('PDBFINDER2','http://swift.cmbi.umcn.nl/gv/pdbfinder/',
+        'ftp://ftp.cmbi.umcn.nl/pub/molbio/data/pdbfinder2/PDBFIND2.TXT.gz',
         re.compile(r'ID           : ([\w]{4})'),LINE,'PDBFINDER'))
     docs.append(_create_databank('NMR','http://www.bmrb.wisc.edu/',
         'ftp://ftp.wwpdb.org/pub/pdb/data/structures/all/nmr_restraints/${PDBID}.mr.gz',
@@ -62,32 +62,32 @@ def create_databanks():
     docs.append(_create_databank('STRUCTUREFACTORS','http://www.pdb.org/',
         'ftp://ftp.wwpdb.org/pub/pdb/data/structures/divided/structure_factors/${PART}/r${PDBID}sf.ent.gz',
         re.compile(r'.*/r([\w]{4})sf\.ent\.gz'),FILE,'MMCIF'))
-    docs.append(_create_databank('PDBREPORT','http://swift.cmbi.ru.nl/gv/pdbreport/',
-        'http://www.cmbi.ru.nl/pdbreport/cgi-bin/nonotes?PDBID=${PDBID}',
+    docs.append(_create_databank('PDBREPORT','http://swift.cmbi.umcn.nl/gv/pdbreport/',
+        'http://www.cmbi.umcn.nl/pdbreport/cgi-bin/nonotes?PDBID=${PDBID}',
         re.compile(r'pdbreport\/\w{2}\/(\w{4})\/pdbout\.txt'),FILE,'PDB'))
-    docs.append(_create_databank('PDB_REDO','http://www.cmbi.ru.nl/pdb_redo/',
-        'http://www.cmbi.ru.nl/pdb_redo/cgi-bin/redir2.pl?pdbCode=${PDBID}',
+    docs.append(_create_databank('PDB_REDO','http://www.cmbi.umcn.nl/pdb_redo/',
+        'http://www.cmbi.umcn.nl/pdb_redo/cgi-bin/redir2.pl?pdbCode=${PDBID}',
         re.compile(r'\/\w{2}\/\w{4}\/(\w{4})_final\.pdb'),FILE,'STRUCTUREFACTORS'))
-    docs.append(_create_databank('DSSP_REDO','http://swift.cmbi.ru.nl/gv/dssp/',
-        'ftp://ftp.cmbi.ru.nl/pub/molbio/data/dssp_redo/${PDBID}.dssp',
+    docs.append(_create_databank('DSSP_REDO','http://swift.cmbi.umcn.nl/gv/dssp/',
+        'ftp://ftp.cmbi.umcn.nl/pub/molbio/data/dssp_redo/${PDBID}.dssp',
         re.compile(r'.*/([\w]{4})\.dssp'),FILE,'PDB_REDO'))
 
     for lis in ['dsp','iod','sbh','sbr','ss1','ss2','tau','acc','cal','wat',
                 'cc1','cc2','cc3','chi']:
-        docs.append(_create_databank('WHATIF_PDB_%s' % lis, 'http://swift.cmbi.ru.nl/whatif/',
-            'ftp://ftp.cmbi.ru.nl/pub/molbio/data/wi-lists/pdb/%s/${PDBID}/${PDBID}.%s.bz2' % (lis, lis),
+        docs.append(_create_databank('WHATIF_PDB_%s' % lis, 'http://swift.cmbi.umcn.nl/whatif/',
+            'ftp://ftp.cmbi.umcn.nl/pub/molbio/data/wi-lists/pdb/%s/${PDBID}/${PDBID}.%s.bz2' % (lis, lis),
             re.compile(r'.*/([\w]{4})\.' + lis + r'(\.bz2)?$'),FILE,'PDB'))
-        docs.append(_create_databank('WHATIF_REDO_%s' % lis, 'http://swift.cmbi.ru.nl/whatif/',
-            'ftp://ftp.cmbi.ru.nl/pub/molbio/data/wi-lists/redo/%s/${PDBID}/${PDBID}.%s.bz2' % (lis, lis),
+        docs.append(_create_databank('WHATIF_REDO_%s' % lis, 'http://swift.cmbi.umcn.nl/whatif/',
+            'ftp://ftp.cmbi.umcn.nl/pub/molbio/data/wi-lists/redo/%s/${PDBID}/${PDBID}.%s.bz2' % (lis, lis),
             re.compile(r'.*/([\w]{4})\.' + lis + r'(\.bz2)?$'),FILE,'PDB_REDO'))
 
     scenames = { 'ss2': 'sym-contacts', 'iod': 'ion-sites'}
     for lis in scenames:
-        docs.append(_create_databank('PDB_SCENES_%s' % lis, 'http://www.cmbi.ru.nl/pdb-vis/',
-            'ftp://ftp.cmbi.ru.nl/pub/molbio/data/wi-lists/pdb/scenes/%s/${PDBID}/${PDBID}_%s.sce' % (lis, scenames[lis]),
+        docs.append(_create_databank('PDB_SCENES_%s' % lis, 'http://www.cmbi.umcn.nl/pdb-vis/',
+            'ftp://ftp.cmbi.umcn.nl/pub/molbio/data/wi-lists/pdb/scenes/%s/${PDBID}/${PDBID}_%s.sce' % (lis, scenames[lis]),
             re.compile(r'.*/([\w]{4})_' + scenames[lis] + r'\.sce'),FILE,'WHATIF_PDB_%s' % lis))
-        docs.append(_create_databank('REDO_SCENES_%s' % lis, 'http://www.cmbi.ru.nl/pdb-vis/',
-            'ftp://ftp.cmbi.ru.nl/pub/molbio/data/wi-lists/redo/scenes/%s/${PDBID}/${PDBID}_%s.sce' % (lis, scenames[lis]),
+        docs.append(_create_databank('REDO_SCENES_%s' % lis, 'http://www.cmbi.umcn.nl/pdb-vis/',
+            'ftp://ftp.cmbi.umcn.nl/pub/molbio/data/wi-lists/redo/scenes/%s/${PDBID}/${PDBID}_%s.sce' % (lis, scenames[lis]),
             re.compile(r'.*/([\w]{4})_' + scenames[lis] + r'\.sce'),FILE,'WHATIF_REDO_%s' % lis))
 
     return docs

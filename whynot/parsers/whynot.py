a73bd80ccad1d0ff2ee67ac4327a47cf011dfc80
Coos Baakman <cbaakman@radboudumc.nl>
2018-11-02 15:34:18 +0100

    bugfix

--- whynot/parsers/whynot.py
+++ whynot/parsers/whynot.py
@@ -2,12 +2,12 @@ def parse_whynot(path):
     comment = None
     comments = {}
     with open(path, 'r') as f:
-        for line in lines:
+        for line in f:
             if line.startswith("COMMENT:"):
                 comment = line[8:].strip()
                 if comment not in comments:
                     comments[comment] = []
             elif len(line.strip()) > 0:
-                db, pdbid = line.split(',')
+                db, pdbid = line.strip().split(',')
                 comments[comment].append((db, pdbid))
     return comments
